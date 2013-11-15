26cc3d45b8ada5520c3a703a1bead24be72176d5
Adam Ford <adam.ford.eng@gmail.com>
2013-11-15 14:59:04 +1100

    Added

--- src/com/example/parsefilecachable/MainActivity.java
+++ src/com/example/parsefilecachable/MainActivity.java
@@ -10,9 +10,6 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		getApplication();
-		getApplication();
-		//Test
 	}
 
 	@Override
---
+++ src/com/example/parsefilecachable/ParseFileCache.java
@@ -0,0 +1,5 @@
+package com.example.parsefilecachable;
+
+public class ParseFileCache {
+
+}
